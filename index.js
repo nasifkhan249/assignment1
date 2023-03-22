c2fa28edd5e89a8a460cdeb528e79acc7d4bd59e
Nasif Mustufa Khan <97841869+nasifkhan249@users.noreply.github.com>
2023-03-22 20:06:44 +0600

    secound commit

---
+++ index.js
@@ -0,0 +1,22 @@
+function printTable(num){
+        for(var i =1 ;i<=10;i++){
+            var result = num*i;
+
+            console.log(num + "x" + i + "=" + result);
+            
+        }
+
+}
+
+printTable(1);
+console.log("");
+
+printTable(2);
+console.log("");
+printTable(3);
+console.log("");
+printTable(4);
+console.log("");
+printTable(5);
+console.log("");
+printTable(6);
